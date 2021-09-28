7df2be853f7f2ef54d3a58cc78a705f378133e85
john <83730668+DevJohn2021@users.noreply.github.com>
2021-09-28 18:04:48 -0500

    avance1

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,13 +9,72 @@
           i.fas.fa-info
       h1 Introducción
     
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
-    
-    p.mb-5 La necesidad de mitigar la pandemia por COVID-19 ha obligado a la comunidad científica internacional, a desarrollar una serie de investigaciones y desarrollos tendientes a presentar una vacuna para hacerle frente a esta enfermedad que aumenta día a día su morbi mortalidad en Colombia. Es así, como en diciembre de 2020, la comunidad científica presenta los resultados de varios desarrollos de vacunas, que ofrecen eficacia y seguridad, respaldados por estudios clínicos rigurosos.
 
+    .row.justify-content-center.mb-2.fondo-bg
+      .col-lg-12.px-md-5
+        .row.justify-content-around.fondo-intro.mb-4(style="border-radius: 8px;")
+          .col-md-3.col-sm-1
+            //vacio
+          .col-lg-7.py-md-5.my-5
+            .row.justify-content-center
+              .col-auto.align-self-center
+                figure
+                  img.mx-auto(src='@/assets/curso/img-intro1.svg', alt='imagen ambiental', style='width: 110px;')
+              .col
+                p.mb-0
+                  b(style="color: #008F85;") Apreciado aprendiz 
+                  b-none , bienvenido a esta experiencia de aprendizaje pensada desde la 
+                  b desinfección del cadáver, de acuerdo con técnicas de asepsia, normas de bioseguridad y salud laboral. 
+                  b-none Le sugerimos observar el siguiente recurso educativo para obtener información antes de iniciar su aprendizaje. 
+                  b(style="color: #008F85;") Bienvenido
+                  b-none .
+
+        .row.justify-content-center.mb-4
+          .col-lg-11.px-md-5
+            PasosA.color-primario.mb-5(tipo="n")
+              .row
+                .col-md-12.mb-4.mb-md-0
+                  .tarjeta-avatar-b.mb-0
+                    .tarjeta-avatar-b__img(style='')
+                      img.mx-auto(src='@/assets/curso/img-intro2.svg' alt='AvatarTop' style='width: 86px;')
+                    .tarjeta
+                      .p-4
+                        p.mb-0 El presente componente de formación surge con la intención que usted identifique cuáles son los procesos a considerar para desinfectar un cadáver e instrumentos. La desinfección es el primer paso del proceso del embalsamamiento, ya que luego continúa la preservación química y la tanatoestética.
+              .row
+                .col-md-12.mb-4.mb-md-0
+                  .tarjeta-avatar-b.mb-0
+                    .tarjeta
+                      .p-4.text-start
+                        p.mb-0 La desinfección es el primer paso del proceso de embalsamamiento, ya que luego continúa la preservación química y la tanatoestética.
+                    .tarjeta-avatar-b__img(style='')
+                      img.mx-auto(src='@/assets/curso/img-intro3.svg' alt='AvatarTop' style='width: 96px;')
+                      
+              .row
+                .col-md-12.mb-4.mb-md-0
+                  .tarjeta-avatar-b.mb-0
+                    .tarjeta-avatar-b__img(style='')
+                      img.mx-auto(src='@/assets/curso/img-intro4.svg' alt='AvatarTop' style='width: 80px;')
+                    .tarjeta
+                      .p-4
+                        p.mb-0 Recuerde que la práctica de embalsamamiento supone que todos los cuerpos poseen agentes infecciosos por eso se desarrollan temáticas en este componente relacionadas con  la asepsia y sus técnicas y los momentos de la desinfección esperada en el laboratorio de tanatopraxia.
+              .row
+                .col-md-12.mb-4.mb-md-0
+                  .tarjeta-avatar-b.mb-0
+                    .tarjeta
+                      .p-4.text-start
+                        p.mb-0 Comprendido las temáticas dadas en este componente mantenga presente en sus prácticas el uso de elementos de bioseguridad para cuidar la salud de su entorno y la suya.
+                    .tarjeta-avatar-b__img(style='')
+                      img.mx-auto(src='@/assets/curso/img-intro5.svg' alt='AvatarTop' style='width: 80px;')
+                      
+              .row
+                .col-md-12.mb-4.mb-md-0
+                  .tarjeta-avatar-b.mb-0
+                    .tarjeta-avatar-b__img(style='')
+                      img.mx-auto(src='@/assets/curso/img-intro6.svg' alt='AvatarTop' style='width: 70px;')
+                    .tarjeta
+                      .p-4.text-start
+                        p.mb-0 Para su aprendizaje debe seguir las orientaciones dadas por su instructor y comprender el contenido para así desarrollar conocimientos, habilidades y actitudes que lo lleven a desempeñarse como un buen técnico.
+                      
 
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,7 +7,249 @@
       .titulo-principal__numero
         span 1
       h1 Asepsia y sus técnicas
-    
+
+
+    .row.justify-content-center.mb-
+      .col-lg-5.align-self-center.mb-4
+        figure
+          img.mx-auto(src='@/assets/curso/tema1/img1.svg' alt='imagen ambiental' style='width: 450px;')
+      .col-lg-7.mb-4
+        p.mb-2
+          b La implementación de procedimientos de asepsia son un mecanismo de defensa y ruptura de la cadena de transmisión de infecciones que causan enfermedades humanas. 
+          b-none  Cada método de limpieza, desinfección, esterilización ayudarán a minimizar y reducir el riesgo de contagio de infecciones. 
+          b En el contexto funerario, al desarrollar servicios de salud y similares, es necesario la aplicación de técnicas de asepsia 
+          b-none porque en las fases del traslado, especialmente en el traslado intermedio, el cuerpo fallecido es un agente potencial de infecciones. En este sentido 
+          b el presente apartado surge con la intención que usted identifique técnicas de asepsia y desinfección del cuerpo y utensilios para su correcta aplicación en el contexto laboral. 
+          
+        .cajon.color-secundario.p-4.mb-0
+          p.mb-0 Antes de continuar, es importante que usted se pregunte
+            b ¿qué es la asepsia?
+            b-none ; para ayudarlo con esa respuesta es importante que se detenga en esta definición. 
+
+
+
+    .row.justify-content-around.fondo1.mb-4(style="border-radius: 8px;")
+      .col-lg-7.py-md-5.my-5
+        .row.justify-content-center
+          .col-auto.align-self-center
+            figure
+              img.mx-auto(src='@/assets/curso/img-intro1.svg', alt='imagen ambiental', style='width: 110px;')
+          .col
+            p.mb-0
+              b(style="color: #008F85;") La asepsia 
+              b-none es un 
+              b proceso por el cual se trata de eliminar o disminuir la presencia de microorganismos que se encuentran alojados sobre objetos, superficies inanimadas, instrumental, entre otros.
+              b-none Su 
+              b(style="color: #008F85;") principal objetivo
+              b-none es evitar que los microorganismos sobre los que se actúa se conviertan en una fuente de contaminación.
+      .col-md-3.col-sm-1
+        //vacio
+
+    p.mb-4 si bien, es 
+      b fundamental comprender el significado de las acciones que se desarrollan en el contexto de la asepsia, 
+      b-none este trasciende cuando usted se implementan un conjunto de procedimientos y recursos, es decir una técnica; 
+      b(style="color: #008F85;") se ha preguntado qué es una técnica en el contexto de la asepsia
+      b-none , si usted acude a varias fuentes de información avaladas académicamente puede coincidir con esta:
+
+    .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-3.mb-4.mb-lg-0
+          figure
+              img.mx-auto(src='@/assets/curso/tema1/img2.svg', alt='imagen ambiental', style='width: 235px;')
+        .col-lg-9
+          .bloque-texto-a__texto.p-4
+            p Son el 
+              b conjunto de actividades y procedimientos realizados con el fin de prevenir y controlar las posibilidades de contaminación microbiana en un sitio de trabajo.
+              b-none En el contexto funerario, es importante que usted 
+              b(style="color: #008F85;") mantenga adecuadas técnicas en el laboratorio de tanatopraxia porque es un lugar donde suelen habitar diferentes microorganismos 
+              b-none por las diferentes acciones que se ejecutan con el cuerpo de un fallecido.
+              
+
+    p.mb-5 En la actividad que suele realizarse de
+      b  embalsamamiento con un cuerpo fallecido dentro del laboratorio de tanatopraxia, 
+      b-none se distinguen algunas 
+      b contaminaciones que puede sufrir el tanatopractor. 
+      b(style="color: #008F85;") La primera es la biológica, 
+      b-none  esta suele caracterizarse por el contacto del funcionario con micro y macroorganismos con cualidades patogénicas que suelen ser tóxicas y desencadenan enfermedades. Mientras que 
+      b(style="color: #008F85;") la segunda es química, 
+      b-none y se refiere a todas aquellas sustancias almacenadas y utilizadas en la preparación de cadáveres, desinfección, desinsectación y combustión. Con el fin de minimizar el riesgo existente al momento de realizar el proceso de embalsamamiento se definen 
+      b medidas básicas de asepsia como lo son:
+
+
+    .row.justify-content-center.mb-2.fondo-bg2
+      .col-lg-12.px-md-5
+        .row.justify-content-center
+          .col-auto.mb-4.px-2
+            .tarjeta.tarjeta-container-Larga.tarjeta-larga2
+              .tarjetaLarga-basic1
+                .tarjetaImgL
+                  figure.mb-3
+                    img.mx-auto(src="@/assets/curso/tema1/img3.jpg", alt="icono", style="width:298px;")
+                  .px-3
+                    .bg-num1
+                        h4.mb-2 Limpieza
+                        p.pb-5 Consiste en desechar la materia orgánica de los elementos o superficies, para ello se utiliza agua y detergente.
+          
+          .col-auto.mb-4.px-2
+            .tarjeta.tarjeta-container-Larga.tarjeta-larga1
+              .tarjetaLarga-basic1
+                .tarjetaImgL
+                  figure.mb-3
+                    img.mx-auto(src="@/assets/curso/tema1/img4.jpg", alt="icono", style="width:298px;")
+                  .px-3
+                    .bg-num2
+                        h4.mb-2 Desinfección
+                        p.pb-5 Consiste en eliminar los microorganismos presentes en objetos u otras superficies mediante el uso de productos químicos desinfectantes.
+                
+          .col-auto.mb-4.px-2
+            .tarjeta.tarjeta-container-Larga.tarjeta-larga2
+              .tarjetaLarga-basic1
+                .tarjetaImgL
+                  figure.mb-3
+                    img.mx-auto(src="@/assets/curso/tema1/img5.jpg", alt="icono", style="width:298px;")
+                  .px-3
+                    .bg-num3
+                        h4.mb-2 Esterilización
+                        p.pb-5 Se define como la liberación de microorganismos de un producto o superficie. 
+                        
+          .col-auto.mb-4.px-2
+            .tarjeta.tarjeta-container-Larga.tarjeta-larga1
+              .tarjetaLarga-basic1
+                .tarjetaImgL
+                  figure.mb-3
+                    img.mx-auto(src="@/assets/curso/tema1/img6.jpg", alt="icono", style="width:298px;")
+                  .px-3
+                    .bg-num4
+                        h4.mb-2 Elementos de protección personal
+                        p.pb-5 Son un conjunto de mecanismos y accesorios externos que generan una barrera entre un riesgo y la salud de una persona.
+                        
+        .row.justify-content-center
+          .col-lg-10
+            LineaTiempoD.color-primario
+              .row(numero="1" titulo="Descontaminación")
+                .col-md-12.mb-4.mb-md-0
+                  p Es el proceso utilizado para disminuir la carga patógena de los objetos que se encuentran supuestamente contaminados ya sea por bacterias, virus u hongos, mediante el uso de sustancias de efecto biocida. Este término engloba los siguientes procesos tanto de limpieza, esterilización como desinfección y sus niveles de desinfección.
+                    
+              .row(numero="2" titulo="Limpieza")
+                .col-md-12.mb-4.mb-md-0
+                  p.mb-4 Es el proceso que se realiza a los instrumentos y equipos con el fin de eliminar la suciedad, empleando métodos físicos o químicos para tal fin. Existen diferentes tipos de limpieza, son:
+                  .row.justify-content-center.mb-2
+                    .col-auto.mb-4
+                      figure
+                        img.mx-auto(src='@/assets/curso/tema1/img7.svg', alt='tipos de limpieza', style='width:255px;')
+                    .col.mb-4.align-self-center
+                      ul.lista-ul.mb-4.ms-md-4
+                        li 
+                          i.fas.fa-biohazard
+                          p.mb-0 #[strong Rutinaria, ] es la limpieza que se realiza en forma diaria con la aplicación de las técnicas básicas de limpieza.
+                        li 
+                          i.fas.fa-biohazard
+                          p.mb-0 #[strong General, ] es aquella limpieza que se realiza en profundidad en las superficies de uso cotidiano.
+                        li 
+                          i.fas.fa-biohazard
+                          p.mb-0 #[strong Terminal, ] es la limpieza realizada al finalizar un proceso.
+                        li 
+                          i.fas.fa-biohazard
+                          p.mb-0 #[strong Especial, ] aquella que se realiza en situaciones excepcionales.
+                      p El instrumental de tanatopraxia se deberá lavar manualmente. Los instrumentos pequeños, finos y delicados tales como agujas, mangos de bisturí, cánulas, exploradores y demás instrumentos que cumplan con esta característica se recomienda lavarse con cepillos abrasivos o esponjas.  En el caso de la máquina de inyección y demás aparatos que cuenten con partes eléctricas (motores, cables, entre otros) se recomienda realizar la limpieza con un paño humedecido en la solución limpiadora.
+                  
+                  .row.justify-content-center.mb-2
+                    .col.mb-4.align-self-center
+                      h4.mb-4 ¿Qué tipo de productos se utilizan para la limpieza? 
+                      p.mb-4 Regularmente se usan detergentes enzimáticos y estos productos deben diluirse en agua fría o tibia para evitar la emisión de vapores y que los restos biológicos queden adheridos sobre el instrumental. Recordemos que el agua caliente coagula la sangre y es mucho más difícil de eliminar. 
+                      p.mb-0 Para la correcta limpieza de los aparatos e instrumental usado en la sala de preservación se debe de realizar el siguiente paso a paso:
+                    .col-auto.mb-4.align-self-center
+                      figure
+                        img.mx-auto(src='@/assets/curso/tema1/img8.svg', alt='imagen ambiental', style='width:132px;')
+                  
+                  .row.justify-content-center.mb-2
+                    .col-auto.mb-2.align-self-center
+                      figure
+                        img.mx-auto(src='@/assets/curso/tema1/img9.svg', alt='imagen ambiental', style='width:145px;')
+                    .col-lg-7.mb-2
+                      ol.lista-ol--cuadro.lista-ol--separador
+                        li 
+                          .lista-ol--cuadro__vineta
+                            span #[strong 1]
+                          p.mb-0 Poner en remojo el material en un recipiente con agua fría o tibia.
+                        li 
+                          .lista-ol--cuadro__vineta
+                            span #[strong 2 ]
+                          p.mb-0 Lavar el instrumental u objeto con detergente.
+                        li 
+                          .lista-ol--cuadro__vineta
+                            span #[strong 3]
+                          p.mb-0 Enjuagar con abundante agua.
+                        li 
+                          .lista-ol--cuadro__vineta
+                            span #[strong 4]
+                          p.mb-0 Secar y prepararlo para la desinfección o esterilización.  
+              
+              
+              .row(numero="3" titulo="Esterilización o desinfección de alto nivel")
+                .col-md-12.mb-4.mb-md-0
+                  .row.justify-content-center
+                    .col-lg-8.mb-4
+                      p.mb-4 La esterilización es un proceso por el cual se destruyen todos los microorganismos viables presentes en un objeto o superficie, incluidas las esporas bacterianas. Para lograr una correcta esterilización lo primero que se debe de realizar es la limpieza exhaustiva del material a esterilizar, que lleva a la eliminación de la suciedad visible y la materia orgánica que se encuentra alojada en una superficie u objeto, reduciendo así el número de microorganismos.
+                    .col-lg-4.mb-4
+                      figure
+                        img.mx-auto(src='@/assets/curso/tema1/img10.svg', alt='imagen ambiental', style='width:139px;')
+                  .row.justify-content-center.mb-4
+                    .col-lg-12
+                      p.mb-0 Existen diferentes métodos para lograr la esterilización, pero la escogencia de un método dependerá de las especificaciones del fabricante quien determina si el material se si puede o no se puede reprocesar. Los métodos más usados de esterilización son físicos y químicos. Veamos sus especificaciones. 
+                  .row.justify-content-center.mb-4
+                    .col-lg-12
+                      .tabla-a.color-primario.mb-5 
+                        table
+                          thead
+                            tr
+                              th(style='width:15%') Tipo de esterilizaci&oacute;n
+                              th(style='width:15%') M&eacute;todo de esterilizaci&oacute;n
+                              th Ventajas
+                              th Desventajas
+                          tbody
+                            tr
+                              td.text-center(rowspan='3') F&iacute;sico
+                              td Vapor 
+                              td
+                                | No es t&oacute;xico.
+                                br
+                                | R&aacute;pido efecto biocida.
+                                br
+                                | Ciclo r&aacute;pido, 20 minutos a 121&ordm;C y a 134&ordm;C entre 3 a 5 minutos
+                              td
+                                | No es apto para el material termosensible.
+                                br
+                                | Riesgo de humedad y de oxidaci&oacute;n.
+                            tr
+                              td Calor Seco
+                              td
+                                | No es corrosivo.
+                                br
+                                | esterilizaci&oacute;n de sustancias en polvo o no acuosas.
+                                br
+                                | Temperatura promedio entre 140&ordm;C y 190&ordm;C.
+                              td Mayor tiempo de esterilizaci&oacute;n 
+                            tr
+                              td Radiaci&oacute;n ionizante 
+                              td
+                                | m&eacute;todo muy eficiente en la desactivaci&oacute;n de microorganismos.
+                                br
+                                | Es r&aacute;pido, flexible y tiene gran capacidad de penetraci&oacute;n en lo materiales.
+                              td
+                                | Solo puede ser aplicada dentro de instalaci&oacute;n especificas con un alto nivel de seguridad.
+
+
+
+              .row(numero="4" titulo="")
+                .col-md-12.mb-4.mb-md-0
+                  
+              
+
+
+
+
+
   
 </template>
 
